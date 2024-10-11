c6b174118560fbc97ec5cd8d1b69f68b67fa0c75
Gustavo <gupaluski@gmail.com>
2024-10-11 16:04:52 -0300

    style: register link

--- doce_conta/lib/screens/register.dart
+++ doce_conta/lib/screens/register.dart
@@ -16,26 +16,40 @@ class RegisterScreen extends StatefulWidget {
 class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.white,
         body: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               //FAZER OS INPUTS COMO UM WIDGET
-              Text("Doce"),
-              Text("Conta", style: TextStyle(color: Colors.green)),
-              Column(
+              const Text("Doce"),
+              const Text("Conta", style: TextStyle(color: Colors.green)),
+              const Column(
                 children: [
                   Text("Mei ou Email"),
                   RegisterInput()
                 ],
               ),
-              Column(
+              const Column(
                 children: [
                   Text("Senha"),
                   RegisterInput()
                 ],
+              ),
+              const ButtonWidget(btnText: "Logar"),
+              RichText(
+                text: const TextSpan(
+                    style: TextStyle(color: Colors.black, fontSize: 12),
+                    children: [
+                      TextSpan(text: "Não possui uma conta? "),
+                      TextSpan(
+                        text: "Cadastra-se",
+                        style: TextStyle(color: Color(0xFF00A3FF))),
+                        //Usar o recognizer para fazer ele ir para outra tela
+                    ]
+                )
               )
             ],
           )
