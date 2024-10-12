e850fe8e5b7ed31208a2ae656b11e34c54c7ae94
Pedro Vitoreti <vitoretipedro@gmail.com>
2024-10-11 22:12:15 -0300

    conf: new db dependency

--- doce_conta/lib/model/connection.dart
+++ doce_conta/lib/model/connection.dart
@@ -1,15 +1,1 @@
-import "package:mysql1/mysql1.dart";
-
-class Database{
-
-  static MySqlConnection? conn;
-  
-  // try {
-  //   conn = await ;
-  // }on MySqlException catch (e, s) {
-  //   throw ();
-  // } finally {
-  //   await conn?.close();
-  // }
-
-}
+import 'package:sqflite/sqflite.dart';
