1174ff1bf8d7dda5d20eced64b89a7289d96ad98
Pedro Vitoreti <vitoretipedro@gmail.com>
2024-10-12 14:15:17 -0300

    conf: merge soluction, but main was lost

---
+++ doce_conta/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:doce_conta/view/pagina_produto.dart';
+import 'package:doce_conta/view/register.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: const RegisterScreen(),
+    );
+  }
+}
