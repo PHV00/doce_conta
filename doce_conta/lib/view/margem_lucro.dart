11699e2d626280957f852f1f86a2ebc8e98037fa
GUSTAVO MAFRA PALUSKI <gustavo_paluski@estudante.sesisenai.org.br>
2024-10-22 10:33:11 -0300

    fix: Add column with profits

--- doce_conta/lib/view/margem_lucro.dart
+++ doce_conta/lib/view/margem_lucro.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import '../widgets/btn_margem_lucro.dart';
 
-void main(){
-  runApp(MargemLucroScreen());
+void main() {
+  runApp(const MargemLucroScreen());
 }
 
 class MargemLucroScreen extends StatelessWidget {
@@ -11,26 +11,44 @@ class MargemLucroScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SafeArea(child: 
-        Scaffold(
+        home: SafeArea(
+      child: Scaffold(
           body: Center(
-            child: Column(
-            children: [
-              const Text("Selecione a procentagem de lucro que gostaria de ter em seu produto:"),
-              for(int i=0; i<8; i++){
-                Row(
-                  children: [
-                    BtnMargemLucro(percentage: "5%",),
-                    BtnMargemLucro(percentage: "10%",),
-                    BtnMargemLucro(percentage: "50%",),
-                  ],
-                ),
-              }
-            ],
-          ),
-          )
+        child: Column(
+          children: [
+            const Text(
+                "Selecione a procentagem de lucro que gostaria de ter em seu produto:"),
+            buildColumnProfits(),
+          ],
         ),
-      )
-    );
+      )),
+    ));
   }
+}
+
+Column buildColumnProfits() {
+  List<Widget> listRow = [];
+  List<Widget> listColumn = [];
+  var j = 0;
+  int index = 0;
+
+  for (var i = 0; i < 8; i++) {
+    while (index < 3) {
+      j++;
+      listRow.add(BtnMargemLucro(percentage: "${j * 5}%"));
+      listRow.add(const SizedBox(
+        width: 50,
+      ));
+      index++;
+    }
+    listRow.removeLast(); //Remove last widht box
+    listRow.add(const SizedBox(height: 60,)); //Add space box height
+    listColumn.add(Row(children: listRow));
+
+    listRow = [];
+    index = 0;
+  }
+  return Column(
+    children: listColumn,
+  );
 }
