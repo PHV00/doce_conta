19ae059bc240cc318027a9411c3d1c388c242b04
Pedro Vitoreti <vitoretipedro@gmail.com>
2024-10-26 09:41:57 -0300

    feat:ButtonWidget can be now execute a void functions, when this is passed in your params

--- doce_conta/lib/view/login.dart
+++ doce_conta/lib/view/login.dart
@@ -1,3 +1,4 @@
+import 'package:doce_conta/view/pagina_produto.dart';
 import 'package:doce_conta/view/register.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
@@ -22,44 +23,67 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    backgroundColor: Colors.white,
-    body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SvgPicture.asset("img/doce_conta_logo.svg", width: 50, height: 50,),
-          const SizedBox(height: 50,),
-
-          const InputAndLabelWidget(labelText: "Mei ou Email",),
-          const SizedBox(height: 20,),
-
-          const InputAndLabelWidget(labelText: "Senha"),
-          const SizedBox(height: 50,),
-          
-          const ButtonWidget(btnText: "Logar"),
-          const SizedBox(height: 10,),
-          RichText(
-            text: TextSpan(
-                style: const TextStyle(color: Colors.black, fontSize: 12),
-                children: [
+        backgroundColor: Colors.white,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              "img/doce_conta_logo.svg",
+              width: 50,
+              height: 50,
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            const InputAndLabelWidget(
+              labelText: "Mei ou Email",
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const InputAndLabelWidget(labelText: "Senha"),
+            const SizedBox(
+              height: 50,
+            ),
+            ButtonWidget(
+              btnText: "Logar",
+              onPressedFunction: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          const ProductPage()));
+              },
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            RichText(
+                text: TextSpan(
+                    style: const TextStyle(color: Colors.black, fontSize: 12),
+                    children: [
                   const TextSpan(text: "Não possui uma conta? "),
                   TextSpan(
-                    recognizer: TapGestureRecognizer()..onTap = () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const RegisterScreen())
-                      );
-                    },
-                    text: "Cadastra-se",
-                    style: const TextStyle(color: Color(0xFF00A3FF))),
-                ]
-            )
-          ),
-          const SizedBox(height: 50,),
-          const OtherOptionsWidget(),
-          const SizedBox(height: 40,),
-          const ApiBrandsLogin()
-        ],
-      )
-    );
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      const RegisterScreen()));
+                        },
+                      text: "Cadastra-se",
+                      style: const TextStyle(color: Color(0xFF00A3FF))),
+                ])),
+            const SizedBox(
+              height: 50,
+            ),
+            const OtherOptionsWidget(),
+            const SizedBox(
+              height: 40,
+            ),
+            const ApiBrandsLogin()
+          ],
+        ));
   }
 }
