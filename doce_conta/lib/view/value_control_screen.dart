78f6c4fd6060e842b0f1ebeafa29f8a22bdcf228
GUSTAVO MAFRA PALUSKI <gustavo.paluski@catolicasc.edu.br>
2024-10-28 20:38:39 -0300

    fix: box size text

--- doce_conta/lib/view/value_control_screen.dart
+++ doce_conta/lib/view/value_control_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+void main() {
+  runApp(ValueControlScreen());
+}
+
 class ValueControlScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -47,7 +51,6 @@ class ValueControl extends StatelessWidget {
               ),
             ),
             SizedBox(height: 16),
-
             Expanded(
               child: GridView.count(
                 crossAxisCount: 2,
@@ -56,15 +59,17 @@ class ValueControl extends StatelessWidget {
                 children: [
                   buildItemCard('Bolos', 'R\$350,00', 'assets/Images/bolo.svg'),
                   buildItemCard('Pães', 'R\$100,00', 'assets/Images/pao.svg'),
-                  buildItemCard('Doces', 'R\$80,00', 'assets/Images/brigadeiro.svg'),
-                  buildItemCard('Salgados', 'R\$80,00', 'assets/Images/croissant.svg'),
-                  buildItemCard('Suco Natural', 'R\$80,00', 'assets/Images/drink2.svg'),
+                  buildItemCard(
+                      'Doces', 'R\$80,00', 'assets/Images/brigadeiro.svg'),
+                  buildItemCard(
+                      'Salgados', 'R\$80,00', 'assets/Images/croissant.svg'),
+                  buildItemCard(
+                      'Suco Natural', 'R\$80,00', 'assets/Images/drink2.svg'),
                   buildItemCard('Café', 'R\$80,00', 'assets/Images/cafe.svg'),
                 ],
               ),
             ),
             SizedBox(height: 16),
-
             ElevatedButton(
               style: ElevatedButton.styleFrom(
                 backgroundColor: const Color(0xFF00382B),
@@ -74,7 +79,8 @@ class ValueControl extends StatelessWidget {
                 ),
               ),
               onPressed: () {},
-              child: Text('Caixa', style: TextStyle(fontSize: 20, color: Colors.white)),
+              child: Text('Caixa',
+                  style: TextStyle(fontSize: 20, color: Colors.white)),
             ),
           ],
         ),
@@ -100,21 +106,22 @@ class ValueControl extends StatelessWidget {
           SvgPicture.asset(
             assetPath,
             color: Colors.orange,
-            height: 70,
+            height: 40,
           ),
-          SizedBox(height: 16),
+          SizedBox(height: 10),
           Container(
             padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
             decoration: BoxDecoration(
               color: const Color.fromARGB(255, 212, 241, 240),
               borderRadius: BorderRadius.circular(20),
-              border: Border.all(color: const Color.fromARGB(255, 191, 228, 215), width: 1.5),
+              border: Border.all(
+                  color: const Color.fromARGB(255, 191, 228, 215), width: 1.5),
             ),
             child: Text(
               'Valor Bruto\n$price',
               textAlign: TextAlign.center,
               style: TextStyle(
-                color:  const Color(0xFF00382B),
+                color: const Color(0xFF00382B),
                 fontSize: 16,
                 fontWeight: FontWeight.bold,
               ),
