5d117936fdb8e7dda124776b5f3d14e47f00bc24
Amanda <amanda.korczagin@gmail.com.br>
2024-11-08 20:22:12 -0300

    Fixing other stuff

--- doce_conta/lib/widgets/api_brands_login.dart
+++ doce_conta/lib/widgets/api_brands_login.dart
@@ -10,10 +10,6 @@ class ApiBrandsLogin extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         SvgPicture.asset("assets/Images/google_icon.svg", width: 24, height: 24,),
-        const SizedBox(width: 40,),
-        SvgPicture.asset("assets/Images/microsoft_icon.svg", width: 24, height: 24,),
-        const SizedBox(width: 40,),
-        SvgPicture.asset("assets/Images/apple_icon.svg", width: 30, height: 30,),
       ],
     );
   }
